7d7d6addda22434a073defdc213a0bfdbf49e307
vvfree <yyang68686@gmail.com>
2025-04-22 23:27:49 +0800

    Site updated: 2025-04-22 23:27:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/22/hello-world/"];function toRandomPost(){
+var posts=["2025/04/22/这是第一篇文章/","2025/04/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
